74d6307fa848711b606aa446b5e3cda66a6a3e0b
yqianjiang <y.jiang.psy@gmail.com>
2026-01-18 13:22:49 +0800

    移除不必要的环境变量加载和基础路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
     return {
-      base: '/lianliankan/',
       build: {
         outDir: 'docs',
       },
@@ -14,10 +12,6 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
